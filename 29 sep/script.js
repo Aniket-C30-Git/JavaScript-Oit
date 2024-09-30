20bca57c29cbcede8f63918767d942a9c97251d2
Aniket-C30-Git <aniketchavan5606@gmail.com>
2024-09-30 11:37:51 +0530

    Add files via upload

---
+++ 29 sep/script.js
@@ -0,0 +1,50 @@
+// let element = document.getElementById("hero")
+// console.log(element);
+
+// let parent = element.parentElement
+// console.log(parent);
+
+// let child = parent.children
+
+// let li3 = childs[2]
+
+// for(let c of childs){
+//     console.log(c);
+// }
+
+
+//let e = document.getElementById("hero");
+// let prev = e.previousElementSibling;
+// let next = e.nextElementSibling;
+//let secNext = e.nextElementSibling.nextElementSibling
+//e.innerHTML = "<strong>Hello i am New Text</strong>"//change in html text 
+
+
+// let e = document.getElementById("hero");
+// let target = e.nextElementSibling.nextElementSibling
+// target.innerHTML = "<emp>i am a man </emp>"
+//=============================================================================
+// let ele = document.getElementById("hero")
+
+// let parent = ele.parentElement
+
+// let parentbro = parent.nextElementSibling
+
+// let target = parentbro.lastElementChild
+// console.log(target);
+
+//ele.parentElement.nextElementSibling.lastElementChild
+//<li>​::marker​"Text d"</li>​//optional Answer in same way
+//===========================================================================
+// let e = document.getElementById("hero")
+// let p = e.parentElement
+// let pb = p.nextElementSibling
+// let c2 = pb.children[1]
+// c2.innerText = "Change Text"
+//===========================================================================
+
+let body = document.body
+let box = body.firstElementChild
+let p = box.lastElementChild
+let c = p.children
+c[2].innerText = "Aniket"
