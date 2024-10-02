e0536592ebbbda77fb1a04855e69db8e2da556d7
Aniket-C30-Git <aniketchavan5606@gmail.com>
2024-10-02 13:03:27 +0530

    Add files via upload

---
+++ 2 October/script.js
@@ -0,0 +1,18 @@
+function ShowWhenMouseEnter(){
+    console.log("Mouse enter in box");
+}
+
+function ShowWhenMouseleave(){
+    console.log("Mouse Leave in box");
+}
+
+// function IsClicked(){
+//     console.log("get click on box");
+// }
+function isDblClicked(){
+    console.log("get double clicked on box");
+}
+
+function isRightClick(){
+    console.log("you make Right click on Mouse");
+}
