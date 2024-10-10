d51a5a988d764fc5a8fe37d006ce15c58b53b708
Aniket-C30-Git <aniketchavan5606@gmail.com>
2024-10-10 13:46:47 +0530

    Add files via upload

---
+++ 10 October/script.js
@@ -0,0 +1,25 @@
+// console.log("Hello");
+// console.log(window);
+// //window is default object
+
+// window.console.log("hello");
+
+// function ABC(){
+//     console.log("i am abc");
+// }
+
+//======== Hoisting In a JS ===========================================
+//(i)This means you can use variables and functions before declaring them in your code.
+//(ii)Variables declared with var are hoisted to the top, but they are not initializations, resulting in undefined if accessed before declaration.
+
+// console.log(A_Aniket);
+// var A_Aniket = 0;
+
+//===================================
+// sayHello();
+
+// function sayHello(){
+//     console.log("hello");
+// }
+
+console.log(screen);
