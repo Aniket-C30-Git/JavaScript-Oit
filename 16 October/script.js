c9a750b684360a3d3c3fab37e01f8a6b425e695b
Aniket-C30-Git <aniketchavan5606@gmail.com>
2024-10-16 13:50:27 +0530

    Add files via upload

---
+++ 16 October/script.js
@@ -0,0 +1,28 @@
+// // console.log("first line");
+// // function print(){
+// //     console.log("second line");
+// // }
+// //print();
+// //setTimeout(print,1000);
+// // var id = setInterval(print,2000); //clearInterval(id) "for clear a id"
+// // console.log("last line");
+// //---------------------------------------------
+
+// //Ex.1)
+// let counter = Number(prompt("Enter Time"));
+// let count = document.querySelector(".count")
+// count.innerHTML = counter
+
+// // function CounterPP(){
+// //     counter++;
+// //     count.innerHTML = counter;
+// // }
+
+// function CounterMM(){
+//     counter--;
+//     count.innerHTML = counter;
+//     if(counter == 0){
+//         clearInterval(id);
+//     }
+// }
+// var id = setInterval(CounterMM,1000)
